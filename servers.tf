87730fb09ccbb7f9ccd187ee67f0ccea15ead0f0
meghasyam1997 <meghasyam01@gmail.com>
2023-06-07 12:18:31 +0530

    making datasource for ami

--- servers.tf
+++ servers.tf
@@ -1,3 +1,13 @@
+data "aws_ami" "centos" {
+  owners           = ["973714476881"]
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+}
+
+output "AMI_ID" {
+  value = data.aws_ami.centos.id
+}
+
 resource "aws_instance" "frontend" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
