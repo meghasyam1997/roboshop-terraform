efd72ffd5e711f7711c0b423cb9b4951936848c9
meghasyam1997 <meghasyam01@gmail.com>
2023-06-09 15:27:17 +0530

    creating route53 use for each

--- servers.tf
+++ servers.tf
@@ -8,10 +8,6 @@ data "aws_security_group" "allow_all" {
   name = "allow-all"
  }
 
-variable "instance" {
-  default = "t3.micro"
-}
-
 variable "components" {
   default = {
     frontend = {
@@ -67,176 +63,12 @@ resource "aws_instance" "instance" {
     Name = each.value["name"]
   }
 }
-/*
 
-
-
-resource "aws_route53_record" "frontend" {
+resource "aws_route53_record" "records" {
+  for_each = var.components
   zone_id = "Z06713411IASYL5XZHSG8"
-  name    = "frontend-dev.msdevops72.online"
+  name    = "${each.value["name"]}-dev.msdevops72.online"
   type    = "A"
   ttl     = 30
-  records = [aws_instance.frontend.private_ip]
+  records = [aws_instance.instance[each.value["name"]].private_ip]
 }
-
-resource "aws_instance" "mongodb" {
-  ami           = data.aws_ami.centos.id
-  instance_type = var.instance
-  vpc_security_group_ids = [data.aws_security_group.allow_all.id]
-
-  tags = {
-    Name = "mongodb"
-  }
-}
-
-resource "aws_route53_record" "mongodb" {
-  zone_id = "Z06713411IASYL5XZHSG8"
-  name    = "mongodb-dev.msdevops72.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.mongodb.private_ip]
-}
-
-resource "aws_instance" "catalogue" {
-  ami           = data.aws_ami.centos.id
-  instance_type = var.instance
-  vpc_security_group_ids = [data.aws_security_group.allow_all.id]
-
-  tags = {
-    Name = "catalogue"
-  }
-}
-
-resource "aws_route53_record" "catalogue" {
-  zone_id = "Z06713411IASYL5XZHSG8"
-  name    = "catalogue-dev.msdevops72.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.catalogue.private_ip]
-}
-
-resource "aws_instance" "redis" {
-  ami           = data.aws_ami.centos.id
-  instance_type = var.instance
-  vpc_security_group_ids = [data.aws_security_group.allow_all.id]
-
-  tags = {
-    Name = "redis"
-  }
-}
-
-resource "aws_route53_record" "redis" {
-  zone_id = "Z06713411IASYL5XZHSG8"
-  name    = "redis-dev.msdevops72.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.redis.private_ip]
-}
-
-resource "aws_instance" "user" {
-  ami           = data.aws_ami.centos.id
-  instance_type = var.instance
-  vpc_security_group_ids = [data.aws_security_group.allow_all.id]
-
-  tags = {
-    Name = "user"
-  }
-}
-
-resource "aws_route53_record" "user" {
-  zone_id = "Z06713411IASYL5XZHSG8"
-  name    = "user-dev.msdevops72.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.user.private_ip]
-}
-
-resource "aws_instance" "cart"{
-  ami           = data.aws_ami.centos.id
-  instance_type = var.instance
-  vpc_security_group_ids = [data.aws_security_group.allow_all.id]
-
-  tags = {
-    Name = "cart"
-  }
-}
-
-resource "aws_route53_record" "cart" {
-  zone_id = "Z06713411IASYL5XZHSG8"
-  name    = "cart-dev.msdevops72.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.cart.private_ip]
-}
-
-resource "aws_instance" "mysql" {
-  ami           = data.aws_ami.centos.id
-  instance_type = var.instance
-  vpc_security_group_ids = [data.aws_security_group.allow_all.id]
-
-  tags = {
-    Name = "mysql"
-  }
-}
-
-resource "aws_route53_record" "mysql" {
-  zone_id = "Z06713411IASYL5XZHSG8"
-  name    = "mysql-dev.msdevops72.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.mysql.private_ip]
-}
-
-resource "aws_instance" "shipping" {
-  ami           = data.aws_ami.centos.id
-  instance_type = var.instance
-  vpc_security_group_ids = [data.aws_security_group.allow_all.id]
-
-  tags = {
-    Name = "shipping"
-  }
-}
-
-resource "aws_route53_record" "shipping" {
-  zone_id = "Z06713411IASYL5XZHSG8"
-  name    = "shipping-dev.msdevops72.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.shipping.private_ip]
-}
-
-resource "aws_instance" "rabbitmq" {
-  ami           = data.aws_ami.centos.id
-  instance_type = var.instance
-  vpc_security_group_ids = [data.aws_security_group.allow_all.id]
-
-  tags = {
-    Name = "rabbitmq"
-  }
-}
-
-resource "aws_route53_record" "rabbitmq" {
-  zone_id = "Z06713411IASYL5XZHSG8"
-  name    = "rabbitmq-dev.msdevops72.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.rabbitmq.private_ip]
-}
-
-resource "aws_instance" "payment" {
-  ami           = data.aws_ami.centos.id
-  instance_type = var.instance
-  vpc_security_group_ids = [data.aws_security_group.allow_all.id]
-
-  tags = {
-    Name = "payment"
-  }
-}
-
-resource "aws_route53_record" "payment" {
-  zone_id = "Z06713411IASYL5XZHSG8"
-  name    = "payment-dev.msdevops72.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.payment.private_ip]
-}*/
