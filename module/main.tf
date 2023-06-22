b954dba51b5ff3d64d396c313064876ea80375a3
meghasyam1997 <meghasyam01@gmail.com>
2023-06-22 10:11:47 +0530

    adding aws iam role and policy

--- module/main.tf
+++ module/main.tf
@@ -51,3 +51,31 @@ resource "aws_iam_role" "role" {
     tag-key = "${var.components_name}-${var.env}"
   }
 }
+
+resource "aws_iam_role_policy" "ssm-ps-policy" {
+  name = "${var.components_name}-${var.env}-ssm-ps-policy"
+  role = aws_iam_role.role.id
+
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+  {
+    "Sid": "VisualEditor0",
+    "Effect": "Allow",
+    "Action": [
+    "ssm:GetParameterHistory",
+    "ssm:GetParametersByPath",
+    "ssm:GetParameters",
+    "ssm:GetParameter"
+  ],
+    "Resource": "arn:aws:ssm:us-east-1:844746520101:parameter/${var.env}.${var.components_name}.*"
+  },
+  {
+    "Sid": "VisualEditor1",
+    "Effect": "Allow",
+    "Action": "ssm:DescribeParameters",
+    "Resource": "*"
+  }
+  ]
+  })
+}
