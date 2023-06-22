f39ef160581ca2e5fab0b3da98b8515b6513384a
meghasyam1997 <meghasyam01@gmail.com>
2023-06-22 11:48:21 +0530

    aws instance profile

--- module/locals.tf
+++ module/locals.tf
@@ -9,7 +9,7 @@ locals{
 
   app_command=[
     "sudo labauto ansible",
-    "ansible-pull -i localhost, -U https://github.com/meghasyam1997/roboshop-ansible roboshop.yml -e env=${var.env} -e role_name=${var.components_name}"
+    "ansible-pull -i localhost, -U https://github.com/meghasyam1997/roboshop-ansible roboshops.yml -e env=${var.env} -e role_name=${var.components_name}"
   ]
 
 }
