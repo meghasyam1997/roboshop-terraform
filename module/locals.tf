600338071092354f9cd25bdf335fc15fc6ac23e8
meghasyam1997 <meghasyam01@gmail.com>
2023-06-26 15:32:02 +0530

    prometheus & node_exporter for filter

--- module/locals.tf
+++ module/locals.tf
@@ -11,5 +11,12 @@ locals{
     "sudo labauto ansible",
     "ansible-pull -i localhost, -U https://github.com/meghasyam1997/roboshop-ansible roboshops.yml -e env=${var.env} -e role_name=${var.components_name}"
   ]
+  db_tags = {
+    Name = "${var.components_name}-${var.env}"
+  }
+  app_tags = {
+    Name = "${var.components_name}-${var.env}"
+    Monitor = "true"
+  }
 
 }
